b307d310e1bc3531564f024207d5b31f643cb589
ruz76 <jan.ruzicka.vsb@gmail.com>
2017-03-01 14:56:01 +0100

    Measuers

--- src/main/java/cz/vsb/ruz76/piii/Point.java
+++ src/main/java/cz/vsb/ruz76/piii/Point.java
@@ -1,11 +1,27 @@
 package cz.vsb.ruz76.piii;
 
+import java.util.Random;
+
 /**
  * Created by ruz76 on 1.3.2017.
  */
 public class Point {
     private double x;
     private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(double minx, double miny, double maxx, double maxy) {
+        Random r = new Random();
+        //TODO random in defined extent
+        double r1 = r.nextDouble();
+        double r2 = r.nextDouble();
+        x = minx + (r1 * (maxx - minx));
+        y = miny + (r1 * (maxy - miny));
+    }
 
     public double getX() {
         return x;
