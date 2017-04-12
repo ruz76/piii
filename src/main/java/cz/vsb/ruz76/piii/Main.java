1aa25376a7ad78e2fd876274fcf5e40ccb97f158
JR <jan.ruzicka.vsb@gmail.com>
2017-04-12 09:24:05 +0200

    Refactoring

--- src/main/java/cz/vsb/ruz76/piii/Main.java
+++ src/main/java/cz/vsb/ruz76/piii/Main.java
@@ -3,39 +3,13 @@ package cz.vsb.ruz76.piii;
 public class Main {
 
     /**
-     * Spouštěcí metoda pro tesotávní třídy IDW.
+     * Spouštěcí metoda pro testování třídy MeasuresStorage.
      * @param args
      */
     public static void main(String[] args) {
-	    // write your code here
-        //System.out.println(args[0]);
-        /*Point p = new Point(-10, 20);
-        Point p2 = new Point(10, 20);
-        System.out.println(p2.getDistance(p));
 
-        Point p3 = new Point(0, 0);
-        Point p4 = new Point(10, 10);
-        System.out.println(p3.getDistance(p4));
-        */
-
-        /*
-        System.out.println(p);
-        System.out.println(p.toString("WKT"));
-        System.out.println(p.toString("Simple"));
-        System.out.println(p.toString("super"));
-        System.out.println(p.toString("nevim"));
-        //
-        */
-
-        IDW idw = new IDW();
-        idw.readMeasures();
-        //System.out.println(idw.getAverageTemperature());
+        MeasuresStorage.getInstance().readMeasures("measures.csv");
         System.out.println(MeasuresStorage.getInstance().getAverageTemperature());
-        //cisla.datum();
-        //cisla.suma(Integer.parseInt(args[0]));
-        //cisla.printRandom();
-        //cisla.generatePoints(Integer.parseInt(args[0]));
-        //cisla.generateGrid(0.5);
-        //cisla.generateMeasures(50);
+
     }
 }
--- src/main/java/cz/vsb/ruz76/piii/MeasuresStorage.java
+++ src/main/java/cz/vsb/ruz76/piii/MeasuresStorage.java
@@ -1,13 +1,14 @@
 package cz.vsb.ruz76.piii;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by ruz76 on 29.3.2017.
  * Singleton pro pristup ke klekci mereni.
  */
 public class MeasuresStorage extends ArrayList {
-    //TODO
     private static MeasuresStorage ourInstance = new MeasuresStorage();
 
     public static MeasuresStorage getInstance() {
@@ -16,6 +17,12 @@ public class MeasuresStorage extends ArrayList {
 
     private MeasuresStorage() {
     }
+
+    /**
+     * Vrátí průměrnou teplotu z načtených měření
+     *
+     * @return průměrnou teplotu
+     */
 
     public double getAverageTemperature() {
         double sum = 0;
@@ -26,6 +33,70 @@ public class MeasuresStorage extends ArrayList {
 
         }
         return sum / ourInstance.size();
+    }
+
+    /**
+     * Načte měření ze souboru CSV ve struktuře lon;lat;dust;temperature;pressure
+     *
+     * @param csvFile soubor s měřeními ve struktuře lon;lat;dust;temperature;pressure
+     */
+
+    public void readMeasures(String csvFile) {
+        String line = "";
+        String cvsSplitBy = ";";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+
+                String[] fields = line.split(cvsSplitBy);
+                double x = Double.parseDouble(fields[0]);
+                double y = Double.parseDouble(fields[1]);
+                Point p = new Point(x, y);
+                System.out.println(p);
+
+                double dust = Double.parseDouble(fields[2]);
+                double temperature = Double.parseDouble(fields[3]);
+                double pressure = Double.parseDouble(fields[4]);
+                Measure measure = new Measure(x, y, dust, temperature, pressure);
+
+                ourInstance.add(measure);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Vygeneruje n náhodných měření v Extentu 12 49 19 51
+     *
+     * @param count počet bodů
+     * @param csvFile soubor s měřeními ve struktuře lon;lat;dust;temperature;pressure
+     */
+
+    public void generateMeasures(int count, String csvFile) {
+        Random r = new Random();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(csvFile))) {
+            for (int i = 0; i < count; i++) {
+                double r1 = r.nextDouble();
+                double r2 = r.nextDouble();
+                double r3 = r.nextDouble();
+                double x = 12 + (r1 * 7);
+                double y = 49 + (r2 * 2);
+                Point p = new Point(x, y);
+                Point random_p = new Point(new Extent("Czech Republic"));
+                Measure m = new Measure(x, y);
+                double dust = 20 + (r3 * 500);
+                double temperature = 0 + (r3 * 10);
+                double pressure = 910 + (r3 * 50);
+                bw.write(x + ";" + y + ";" + dust + ";" + temperature + ";" + pressure + System.lineSeparator());
+                //bw.write(x + ";" + y + ";" + dust + ";" + temperature + ";" + pressure + "\N");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
